76215bcd2a81e3b825b34f89af26c8157afd53a7
Kevin De La Torre <kevindelatorre@live.com>
2019-12-16 21:45:51 -0800

    Added POST to /events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,24 @@
 class EventsController < ApplicationController
 
+  # GET /events
   def index
-    @todos = Event.all
-    json_response( @todos )
+    @events = Event.all
+    json_response( @events )
   end
+
+  # POST /events
+  def create
+    @event = Event.create!( event_params )
+    json_response( @event, :created ) # Returns status ok if created
+  end
+
+  private 
+    def event_params
+      params.permit( :title, :description, :people, :location, :category, :start_time, :end_time, :date )
+    end
+
+    def set_event
+      @event = Event.find( params[ :id ] )
+    end
 
 end
