9f2ad79dd642daa16ab528968982722f248e92cd
Luke Walsh <lukewalsh@lwalsh-mbpr19.local>
2020-09-13 21:25:34 -0700

    Kevin you always mess up the spacing lol

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,13 +30,13 @@ class EventsController < ApplicationController
     json_response( @event )
   end
 
-  private 
-    def event_params
-      params.permit( :title, :description, :people, :location, :category, :start_time, :end_time, :date )
-    end
+  private
 
-    def set_event
-      @event = Event.find( params[ :id ] )
-    end
+  def event_params
+    params.permit( :title, :description, :people, :location, :category, :start_time, :end_time, :date )
+  end
 
+  def set_event
+    @event = Event.find( params[ :id ] )
+  end
 end
