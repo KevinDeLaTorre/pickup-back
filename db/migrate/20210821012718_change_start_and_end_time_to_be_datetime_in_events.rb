f62d36a1c6cd8c152c2a1f2edb9bb6b0f4227f05
Luke Walsh <lhw2096@yahoo.com>
2021-08-20 21:08:48 -0500

    Just use change method for migration

--- db/migrate/20210821012718_change_start_and_end_time_to_be_datetime_in_events.rb
+++ db/migrate/20210821012718_change_start_and_end_time_to_be_datetime_in_events.rb
@@ -1,11 +1,6 @@
 class ChangeStartAndEndTimeToBeDatetimeInEvents < ActiveRecord::Migration[6.0]
-  def up
+  def change
     change_column :events, :start_time, :datetime
     change_column :events, :end_time, :datetime
-  end
-
-  def down
-    change_column :events, :start_time, :time
-    change_column :events, :end_time, :time
   end
 end
