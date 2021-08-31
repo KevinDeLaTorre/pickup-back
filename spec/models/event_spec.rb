b39dcfb0fe5cbc5aeabd0185a898939d2f6e379e
Kevin De La Torre <kevindelatorre@live.com>
2021-08-30 23:02:30 -0700

    Added event validation tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  it "is valid with a title, date, and start_time" do
+    event = Event.new(title: "example", date: Date.today, start_time: 1.hour.ago)
+    expect(event).to be_valid
+  end
+
+  it "is not valid without a title" do
+    event = Event.new(title: "", date: Date.today, start_time: 1.hour.ago)
+    expect(event).to_not be_valid
+  end
+
+  it "is not valid without a date" do
+    event = Event.new(title: "example", date: nil, start_time: 1.hour.ago)
+    expect(event).to_not be_valid
+  end
+  it "is not valid without a start time" do
+    event = Event.new(title: "example", date: Date.today, start_time: nil)
+    expect(event).to_not be_valid
+  end
 end
