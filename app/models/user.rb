d5dfeb728055b947b55518180d4b8772546c583d
Kevin De La Torre <kevindelatorre@live.com>
2021-09-03 02:13:05 -0700

    Migrate user model to devise standard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   before_save :downcase_email
 
   validates :name, presence: true, length: { maximum: 50 }
