c84b55a46ac4e2e6974fabca48a143eda91d9622
Kevin De La Torre <kevindelatorre@live.com>
2021-09-03 17:13:49 -0700

    Add isFollowing? function to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,9 @@ class Event < ApplicationRecord
     validates :start_time, presence: true
     has_many :passive_relationships, class_name: "EventLink", foreign_key: "event_id", dependent: :destroy
     has_many :followers, through: :passive_relationships, source: :user
+
+    # Returns whether a user is a follower of this event
+    def isFollower?(user)
+        return followers.include?(user)
+    end
 end
