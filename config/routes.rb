b029596d474f61a6ef801952613f9b4b6dabc15b
Kevin De La Torre <kevindelatorre@live.com>
2021-09-06 16:06:22 -0700

    Remove devise routes and just keep token routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   scope 'api', as: 'api' do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {registrations: 'auth/registrations'}
     resources :events
